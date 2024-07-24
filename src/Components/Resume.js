30f26e56639524cd34f8034bf6303b632e823b7c
VasquezDav <davasquez7@miners.utep.edu>
2024-07-24 13:30:28 -0600

    improving reume demensions

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,7 +8,6 @@ function Resume() {
       <h1>Resume</h1>
       <iframe
         src="/myResume.pdf"
-        style={{ width: "80%", height: "70vh", border: "none" }}
         title="PDF Viewer"
       />
     </div>
