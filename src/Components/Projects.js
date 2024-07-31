77c631557e3f65b7381ad460a452fd913bf4e566
VasquezDav <davasquez7@miners.utep.edu>
2024-07-31 15:58:19 -0600

    Css for project section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Projects() {
+
   return (
-    <div className='projects' id='projects'>
+    <div className="projects" id="projects">
       <h1>Projects</h1>
-      <div className='project-container'>
-        <div className='box spotify-journal'></div>
-        <div className='box arcana'></div>
-        <div className='box uwc-queue>'></div>
-        <div className='box fithub'></div>
+      <div className="project-container">
+        <div className="box spotify-journal">
+          <div className="title-box">
+            <span>Spotify Journal Application</span>
+          </div>
+          <div className="logo"></div>
+        </div>
+        <div className="box arcana">
+          <div className="title-box">
+            <span>
+              Advanced Resiliency Chronicling for Analysis in Network
+              Assessments
+            </span>
+          </div>
+          <div className="logo"></div>
+        </div>
+        <div className="box uwc-queue">
+          <div className="title-box">
+            <span>The University of Texas at El Paso Writing Center Queue App</span>
+          </div>
+          <div className="logo"></div>
+        </div>
+        <div className="box fithub">
+          <div className="title-box"><span>Personal Fitness Web-App</span></div>
+          <div className="logo"></div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
